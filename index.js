4f7e6628ecbaca138498ade87889607ec87f3654
montheramer <monther.amer48@gmail.com>
2021-01-23 14:28:36 +0200

    REMOVE js fiels

--- index.js
+++
@@ -1,7 +0,0 @@
-const { listCountries } = require('./functions/list_country');
-
-const { searchCountries } = require('./functions/search_countries');
-
-const { listCities } = require('./functions/list_cities');
-
-module.exports = { listCountries, searchCountries, listCities };
